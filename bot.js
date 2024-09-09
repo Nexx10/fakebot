e5dc7d6bc8d4db51c00af1ce67506ec83bf3bc00
Meeraxareem <dikatenlucky48@gmail.com>
2024-09-09 10:41:56 +0700

    update

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
+const loaders = document.getElementById("loaders");
+const container = document.getElementsByClassName("container");
 
 let init = 0;
 
@@ -17,24 +19,55 @@ question.innerHTML = botSay()[0];
 let usersData = [];
 
 function botStart() {
+
+  if(answer.value.length < 1) return alert("Isi dulu")
+  
   init++;
-  if (init === 1) {
-    botDelay({ nama: answer.value });
-  } else if (init === 2) {
-    botDelay({ usia: answer.value });
-  } else if (init === 3) {
-    botDelay({ hobby: answer.value });
-  } else if (init === 4) {
-    finishing();
-  } else {
-    botEnd();
+
+  // if (init === 1) {
+  //   botDelay({ nama: answer.value });
+  // } else if (init === 2) {
+  //   botDelay({ usia: answer.value });
+  // } else if (init === 3) {
+  //   botDelay({ hobby: answer.value });
+  // } else if (init === 4) {
+  //   finishing();
+  // } else {
+  //   botEnd();
+  // }
+
+  switch (init) {
+    case 1:
+      botDelay({ nama: answer.value });
+      break;
+
+    case 2:
+      botDelay({ usia: answer.value });
+      break;
+
+    case 3:
+      botDelay({ hobby: answer.value });
+      break;
+
+    case 4:
+      finishing();
+      break;
+
+    case 5:
+      botEnd();
+    default:
+      break;
   }
 }
 
 function botDelay(userAnswer) {
+  loaders.style.display = "block";
+  container[0].style.filter = "blur(8px)";
   setTimeout(() => {
     question.innerHTML = botSay(userAnswer)[init];
-  }, 2000);
+    loaders.style.display = "none";
+    container[0].style.filter = "none";
+  }, 1700);
   usersData.push(answer.value);
   answer.value = "";
 }
@@ -45,5 +78,6 @@ function finishing() {
 }
 
 function botEnd() {
+  alert(`Terimakasih ${usersData[0]} sudah berkunjung, anda akan diarahkan ke halaman utama`);  
   window.location.reload();
 }
